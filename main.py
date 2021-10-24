4847546fa9e7edd7ee557e6a945b1e3a106b1113
Эльдар Эсакаев <eldaresakaev@MacBook-Air-Eldar.local>
2021-10-24 16:51:06 +0300

    parser is done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from datetime import date, datetime
 import time
@@ -13,17 +14,84 @@ def get_first_news():
     r = requests.get(url=url, headers=headers)
 
     soup = BeautifulSoup(r.text, "lxml")
-    articles_card  = soup.find_all("a", class_= "article-card")
+    articles_cards  = soup.find_all("a", class_= "article-card")
 
-    for article in articles_card:
-        articles_title = article.find("h2", class_="article-card-title").text.strip()
+    news_dict = {}
+    for article in articles_cards:
+        article_title = article.find("h2", class_="article-card-title").text.strip()
         article_desc = article.find("p").text.strip()
         article_url = f'https://www.securitylab.ru{article.get("href")}'
         article_date_time = article.find("time").get("datetime")
         date_from_iso = datetime.fromisoformat(article_date_time)
         date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
         article_date_timestamp = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").timetuple())
-    
-        print(f"{articles_title} | {article_url} | {article_date_timestamp}")
 
-get_first_news()
+        article_id = article_url.split("/")[-1]
+        article_id = article_id[:-4]
+
+        #print(f"{article_title} | {article_url} | {article_date_timestamp}")
+        news_dict[article_id] = {
+            "article_date_timestamp": article_date_timestamp,
+            "article_title": article_title,
+            "article_url": article_url,
+            "article_desc": article_desc
+        }
+    with open("news_dict.json", "w") as file:
+        json.dump(news_dict, file, indent=4, ensure_ascii=False)
+
+
+def check_news_update():
+    with open("news_dict.json") as file:
+        news_dict = json.load(file)
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+    }
+    url = "https://www.securitylab.ru/news/"
+
+    r = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(r.text, "lxml")
+    articles_cards  = soup.find_all("a", class_= "article-card")
+    
+    fresh_news = {}
+    for article in articles_cards:
+        article_url = f'https://www.securitylab.ru{article.get("href")}'
+        article_id = "article_url".split("/")[-1]
+        article_id = article_id[:-4]
+        if article_id in news_dict:
+            continue
+        else:
+            article_title = article.find("h2", class_="article-card-title").text.strip()
+            article_desc = article.find("p").text.strip()
+            article_date_time = article.find("time").get("datetime")
+            date_from_iso = datetime.fromisoformat(article_date_time)
+            date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
+            article_date_timestamp = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").timetuple())
+
+        news_dict[article_id] = {
+            "article_date_timestamp": article_date_timestamp,
+            "article_title": article_title,
+            "article_url": article_url,
+            "article_desc": article_desc
+        }
+        
+        fresh_news[article_id] = {
+            "article_date_timestamp": article_date_timestamp,
+            "article_title": article_title,
+            "article_url": article_url,
+            "article_desc": article_desc
+        }
+    
+    with open("news_dict.json", "w") as file:
+        json.dump(news_dict, file, indent=4, ensure_ascii=False)
+    
+    return fresh_news
+
+
+def main():
+    get_first_news()
+    print(check_news_update())
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-url = "https://www.securitylab.ru/news/525841.php"
-article_id = url.split("/")[-1]
-article_id = article_id[:-4]
-print(article_id)
+# url = "https://www.securitylab.ru/news/525841.php"
+# article_id = url.split("/")[-1]
+# article_id = article_id[:-4]
+# print(article_id)
+import json
+
+with open("news_dict.json") as file:
+    news_dict = json.load(file)
+
+search_id = "525887"
+if search_id in news_dict:
+    print('Новость уже есть')
+else:
+    print("Свежая новость, добавляем в словарь")
