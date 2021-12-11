3cb9ed9c62110393da4154a3877f026d7ecde336
Эльдар Эсакаев <eldaresakaev@MacBook-Air-Eldar.local>
2021-12-11 19:24:14 +0300

    basis is done

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-import requests
 import json
+import requests
 from bs4 import BeautifulSoup
-from datetime import date, datetime
+from datetime import datetime
 import time
 
 
 def get_first_news():
     headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
     }
-    url = "https://coinmarketcap.com/new/"v
 
+    url = "https://www.securitylab.ru/news/"
     r = requests.get(url=url, headers=headers)
 
     soup = BeautifulSoup(r.text, "lxml")
-    articles_cards  = soup.find_all("a", class_= "article-card")
+    articles_cards = soup.find_all("a", class_="article-card")
 
     news_dict = {}
     for article in articles_cards:
         article_title = article.find("h2", class_="article-card-title").text.strip()
         article_desc = article.find("p").text.strip()
         article_url = f'https://www.securitylab.ru{article.get("href")}'
+
         article_date_time = article.find("time").get("datetime")
         date_from_iso = datetime.fromisoformat(article_date_time)
         date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
@@ -29,13 +30,15 @@ def get_first_news():
         article_id = article_url.split("/")[-1]
         article_id = article_id[:-4]
 
-        #print(f"{article_title} | {article_url} | {article_date_timestamp}")
+        # print(f"{article_title} | {article_url} | {article_date_timestamp}")
+
         news_dict[article_id] = {
             "article_date_timestamp": article_date_timestamp,
             "article_title": article_title,
             "article_url": article_url,
             "article_desc": article_desc
         }
+
     with open("news_dict.json", "w") as file:
         json.dump(news_dict, file, indent=4, ensure_ascii=False)
 
@@ -45,53 +48,56 @@ def check_news_update():
         news_dict = json.load(file)
 
     headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
     }
-    url = "https://www.securitylab.ru/news/"
 
+    url = "https://www.securitylab.ru/news/"
     r = requests.get(url=url, headers=headers)
 
     soup = BeautifulSoup(r.text, "lxml")
-    articles_cards  = soup.find_all("a", class_= "article-card")
-    
+    articles_cards = soup.find_all("a", class_="article-card")
+
     fresh_news = {}
     for article in articles_cards:
         article_url = f'https://www.securitylab.ru{article.get("href")}'
-        article_id = "article_url".split("/")[-1]
+        article_id = article_url.split("/")[-1]
         article_id = article_id[:-4]
+
         if article_id in news_dict:
             continue
         else:
             article_title = article.find("h2", class_="article-card-title").text.strip()
             article_desc = article.find("p").text.strip()
+
             article_date_time = article.find("time").get("datetime")
             date_from_iso = datetime.fromisoformat(article_date_time)
             date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
             article_date_timestamp = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").timetuple())
 
-        news_dict[article_id] = {
-            "article_date_timestamp": article_date_timestamp,
-            "article_title": article_title,
-            "article_url": article_url,
-            "article_desc": article_desc
-        }
-        
-        fresh_news[article_id] = {
-            "article_date_timestamp": article_date_timestamp,
-            "article_title": article_title,
-            "article_url": article_url,
-            "article_desc": article_desc
-        }
-    
+            news_dict[article_id] = {
+                "article_date_timestamp": article_date_timestamp,
+                "article_title": article_title,
+                "article_url": article_url,
+                "article_desc": article_desc
+            }
+
+            fresh_news[article_id] = {
+                "article_date_timestamp": article_date_timestamp,
+                "article_title": article_title,
+                "article_url": article_url,
+                "article_desc": article_desc
+            }
+
     with open("news_dict.json", "w") as file:
         json.dump(news_dict, file, indent=4, ensure_ascii=False)
-    
+
     return fresh_news
 
 
 def main():
     get_first_news()
     print(check_news_update())
+
 
 if __name__ == '__main__':
     main()
